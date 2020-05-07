7a774900584a34a675a5883ea89316b49b90237a
Raihan Bin Wahid <56556010+Rb-wahid@users.noreply.github.com>
2020-05-07 18:13:50 +0600

    commit

---
+++ Become Java Developer/Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String name = "Rb_wahid";
+        int year = 2020;
+        System.out.println("Hello! My name is " + name + ".\n" +
+                "I was created in " + year + ".");
+    }
+}
